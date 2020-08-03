f30c1e5315bb055d2ff7e16973c916365f3107e5
roldanjrCodeArts9711 <roldanjrmontilla@gmail.com>
2020-08-03 15:14:38 +0800

    update: docs source code

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -6,22 +6,40 @@ function App() {
 
   return (
     <div className="container">
-      <button
-        className={`btn is-primary ${
-          !device.isAllowedToSleep ? "is-active" : ""
-        }`.trim()}
-        onClick={device.preventSleep}
-      >
-        Prevent Sleep
-      </button>
-      <button
-        className={`btn is-secondary ${
-          device.isAllowedToSleep ? "is-active" : ""
-        }`.trim()}
-        onClick={device.allowSleep}
-      >
-        Allow Sleep
-      </button>
+      <header className="header">
+        <h1>use-stay-awake</h1>
+        <p>
+          React hook that will help the device stay awake while actively using
+          your website or app.
+        </p>
+      </header>
+
+      <p className="status">
+        Status:{" "}
+        <span>
+          {device.isAllowedToSleep
+            ? "Device is allowed to sleep"
+            : "Device is not allowed to sleep"}
+        </span>
+      </p>
+      <div className="control">
+        <button
+          className={`btn is-primary ${
+            !device.isAllowedToSleep ? "is-active" : ""
+          }`.trim()}
+          onClick={device.preventToSleep}
+        >
+          Prevent Sleep
+        </button>
+        <button
+          className={`btn is-secondary ${
+            device.isAllowedToSleep ? "is-active" : ""
+          }`.trim()}
+          onClick={device.allowToSleep}
+        >
+          Allow Sleep
+        </button>
+      </div>
     </div>
   );
 }
