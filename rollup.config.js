5b29118b2ea3ace2c1aafaaf15efe61bb39e5459
roldanjrCodeArts9711 <roldanjrmontilla@gmail.com>
2020-08-28 10:40:26 +0800

    update: remove sourcemap to decrease the bundle size

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,12 @@ import typescript from "rollup-plugin-typescript2";
 import url from "@rollup/plugin-url";
 import pkg from "./package.json";
 
-// TODO: try to make the bundle size much smaller using babel
-
 export default {
   input: pkg.source,
   output: [
     {
       file: pkg.main,
       format: "esm",
-      sourcemap: true,
       exports: "default",
     },
   ],
